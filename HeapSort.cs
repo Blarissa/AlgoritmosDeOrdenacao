c8d7e2cb9e0295a51535b47d79d43b3d1cbc57cc
Blarissa <larissabrasil009@gmail.com>
2023-07-03 21:04:49 -0300

    feat: algoritmo de ordenção heap  sort

---
+++ HeapSort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LarissaAtvOrdenação
+{
+    public class HeapSort
+    {
+        public static void Ordenar(int[] vetor)
+        {
+            int n = vetor.Length;
+
+            ConstroiHeapMax(vetor,n);
+
+            for(int i = n - 1; i > 0; i--)
+            {
+                Trocar(vetor, i, 0);
+
+                n --;
+                
+                RefazHeapMax(vetor, i, 0);
+            }
+        }
+
+        private static void RefazHeapMax(int[] vetor, int n, int i)
+        {
+            int maior = i; 
+            int esquerdo = 2 * i + 1; 
+            int direito = 2 * i + 2;
+
+            if (esquerdo < n && 
+                vetor[esquerdo] > vetor[maior])
+                maior = esquerdo;
+            
+            if (direito < n && 
+                vetor[direito] > vetor[maior])
+                maior = direito;
+
+            if (maior != i)
+            {
+                Trocar(vetor, i, maior);
+                RefazHeapMax(vetor, n, maior);
+            }
+        }
+
+        private static void Trocar(int[] vetor, int i, int j)
+        {
+            int aux = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = aux;
+        }
+
+        private static void ConstroiHeapMax(int[] vetor, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                RefazHeapMax(vetor, n, i);
+        }
+    }
+}
